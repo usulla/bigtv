f9e5056c8314d6443aad8a1ea7cdd93a59001f92
Sukhareva Maria <maria_suxareva@mail.ru>
2019-06-13 16:12:01 +0300

    Update table

--- src/components/Table/CustomPaginationActionsTable.jsx
+++ src/components/Table/CustomPaginationActionsTable.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
-import TableHead from '@material-ui/core/TableHead';
+import TableHead from "@material-ui/core/TableHead";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -12,141 +12,239 @@ import TableFooter from "@material-ui/core/TableFooter";
 //import TablePaginationActions from "./TablePaginationActions.jsx";
 
 const useStyles1 = makeStyles(theme => ({
-  root: {
-    flexShrink: 0,
-    color: theme.palette.text.secondary,
-    marginLeft: theme.spacing(2.5)
-  }
+    root: {
+        flexShrink: 0,
+        color: theme.palette.text.secondary,
+        marginLeft: theme.spacing(2.5)
+    }
 }));
 
 function createData(obj) {
-  const rows = {};
-  for (var key in obj){
-      rows.push(obj[key]);
-  }
-  return rows;
+    const rows = {};
+    for (var key in obj) {
+        rows.push(obj[key]);
+    }
+    return rows;
 }
 
 const useStyles2 = makeStyles(theme => ({
-  root: {
-    width: "100%",
-    marginTop: theme.spacing(3)
-  },
-  table: {
-    minWidth: 500
-  },
-  tableWrapper: {
-    overflowX: "auto"
-  }
+    root: {
+        width: "100%",
+        marginTop: theme.spacing(3)
+    },
+    table: {
+        minWidth: 500
+    },
+    tableWrapper: {
+        overflowX: "auto"
+    }
 }));
 
 function CustomPaginationActionsTable(props) {
-  const rows = props.dataForTable;
-  const classes = useStyles2();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(20005);
-  const [selected, setSelected] = React.useState([]);
+    const rows = props.dataForTable;
 
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-  const isSelected = name => selected.indexOf(name) !== -1;
-  console.log(isSelected, 'isSelected')
+    var table1 = rows[0];
+    var table2 = rows[1];
+    console.log(table1, "fgfg");
+    const classes = useStyles2();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(20005);
+    const [selected, setSelected] = React.useState([]);
 
-  function handleSelectAllClick(event) {
-    if (event.target.checked) {
-      const newSelecteds = rows.map(n => n.name);
-      setSelected(newSelecteds);
-      return;
+    const emptyRows =
+        rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    const isSelected = name => selected.indexOf(name) !== -1;
+    console.log(isSelected, "isSelected");
+
+    function handleSelectAllClick(event) {
+        if (event.target.checked) {
+            const newSelecteds = rows.map(n => n.name);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
     }
-    setSelected([]);
-  }
-  function handleClick(event, name) {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
+    function handleClick(event, name) {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected = [];
 
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1)
+            );
+        }
+
+        setSelected(newSelected);
+    }
+    function tableBodyComp() {
+      return <h1>С возвращением!</h1>;
     }
 
-    setSelected(newSelected);
-  }
+    return (
+        <div className={classes.root}>
+            <div className={classes.tableWrapper}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="right" />
+                            <TableCell align="right">IDEC</TableCell>
+                            <TableCell align="right">Дата эфира</TableCell>
+                            <TableCell align="right">Название</TableCell>
+                            <TableCell align="right">Серия</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {table1
+                            .slice(
+                                page * rowsPerPage,
+                                page * rowsPerPage + rowsPerPage
+                            )
+                            .map((row, index) => {
+                                const isItemSelected = isSelected(row.IDEC);
+                                const labelId = `enhanced-table-checkbox-${index}`;
+                                let button2;
+                                if (row.compare === false) {
+                                  button2 = <div><TableRow
+                                  onClick={event =>
+                                      handleClick(event, row.IDEC)
+                                  }
+                                  role="checkbox"
+                                  aria-checked={isItemSelected}
+                                  tabIndex={-1}
+                                  key={row.IDEC}
+                                  selected={isItemSelected}
+                              >
+                                  <TableCell padding="checkbox">
+                                      <Checkbox
+                                          checked={isItemSelected}
+                                          inputProps={{
+                                              "aria-labelledby": labelId
+                                          }}
+                                      />
+                                  </TableCell>
+                                      <TableCell
+                                          component="th"
+                                          id={labelId}
+                                          scope="row"
+                                          padding="none"
+                                      >
+                                          {row.IDEC}
+                                      </TableCell>
+                                      <TableCell align="right">
+                                          {row.datetime}
+                                      </TableCell>
+                                  <TableCell align="right">
+                                      {row.datetime}
+                                  </TableCell>
+                                  <TableCell align="right">
+                                      {row.HOUSE_ID}
+                                  </TableCell>
+                              </TableRow>   
+                              <TableRow
+                              onClick={event =>
+                                  handleClick(event, table2[index].IDEC)
+                              }
+                              role="checkbox"
+                              aria-checked={isItemSelected}
+                              tabIndex={-1}
+                              key={table2[index].IDEC}
+                              selected={isItemSelected}
+                              >
+                              <TableCell padding="checkbox">
+                                  <Checkbox
+                                      checked={isItemSelected}
+                                      inputProps={{
+                                          "aria-labelledby": labelId
+                                      }}
+                                  />
+                              </TableCell>
+                              <TableCell
+                                      component="th"
+                                      id={labelId}
+                                      scope="row"
+                                      padding="none"
+                                  >
+                                      {table2[index].IDEC}
+                                  </TableCell>
+                                  <TableCell align="right">
+                                      {table2[index].datetime}
+                                  </TableCell>
+                              <TableCell align="right">
+                                  {table2[index].datetime}
+                              </TableCell>
+                              <TableCell align="right">
+                                  {table2[index].HOUSE_ID}
+                              </TableCell>
+                          </TableRow>  
+                          </div>
+                                } else {
+                                  button2 =  <TableRow
+                                  onClick={event =>
+                                      handleClick(event, row.IDEC)
+                                  }
+                                  role="checkbox"
+                                  aria-checked={isItemSelected}
+                                  tabIndex={-1}
+                                  key={row.IDEC}
+                                  selected={isItemSelected}
+                              >
+                                  <TableCell padding="checkbox">
+                                      <Checkbox
+                                          checked={isItemSelected}
+                                          inputProps={{
+                                              "aria-labelledby": labelId
+                                          }}
+                                      />
+                                  </TableCell>
+                                      <TableCell
+                                          component="th"
+                                          id={labelId}
+                                          scope="row"
+                                          padding="none"
+                                      >
+                                          {row.IDEC}
+                                      </TableCell>
+                                      <TableCell align="right">
+                                          {row.datetime}
+                                      </TableCell>
+                                  <TableCell align="right">
+                                      {row.datetime}
+                                  </TableCell>
+                                  <TableCell align="right">
+                                      {row.HOUSE_ID}
+                                  </TableCell>
+                              </TableRow>   
+                                }
+                            return(<div>
+                                    {button2}                               
+                            </div>
+                                 ) 
+                            })}
 
-  return (
-    
-    <div className={classes.root}>
-      <div className={classes.tableWrapper}>
-        <Table className={classes.table}>
-         <TableHead>
-            <TableRow>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right">IDEC</TableCell>
-            <TableCell align="right">Дата эфира</TableCell>
-            <TableCell align="right">Название</TableCell>
-            <TableCell align="right">Серия</TableCell>
-          </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, index) => {
-                const isItemSelected = isSelected(row.IDEC);
-                const labelId = `enhanced-table-checkbox-${index}`;
-                return (
-                  <TableRow
-                    onClick={event => handleClick(event, row.IDEC)}
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={row.IDEC}
-                    selected={isItemSelected}
-                  >
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        checked={isItemSelected}
-                        inputProps={{ "aria-labelledby": labelId }}
-                      />
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
-                    >
-                      {row.IDEC}
-                    </TableCell>
-                    <TableCell align="right">{row.datetime}</TableCell>
-                    <TableCell align="right">{row.HOUSE_ID}</TableCell>
-                    <TableCell align="right">{row.HOUSE_ID}</TableCell>
-                  </TableRow>
-                );
-              })}
-
-            {/* {emptyRows > 0 && (
+                        {/* {emptyRows > 0 && (
               <TableRow style={{ height: 48 * emptyRows }}>
                 <TableCell colSpan={6} />
               </TableRow>
             )} */}
-          </TableBody>
-          {/*
+                    </TableBody>
+                    {/*
           <TableFooter>
             <TableRow>
               <TablePaginationActions count={rows.length} rowsPerPage={rowsPerPage} page={page} onChangePage={handleChangePage} onChangeRowsPerPage={handleChangeRowsPerPage}/>
             </TableRow>
           </TableFooter>
           */}
-        </Table>
-      </div>
-    </div>
-  );
+                </Table>
+            </div>
+        </div>
+    );
 }
 
 export default CustomPaginationActionsTable;
